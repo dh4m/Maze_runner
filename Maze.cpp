5c449e3b6fa3e67be47d7207b69d52163096f199
함동균 <dh4m28@gmail.com>
2023-05-31 15:57:16 +0900

    maze

--- Maze.cpp
+++ Maze.cpp
@@ -1,6 +1,8 @@
 #include "Maze.hpp"
+#include <cstdlib>
+#include <ctime>
 
-const int Maze::dir[24][4] = {
+const int Maze::_dir[NUM_DIR][4] = {
 	{1, 2, 3, 4}, {1, 2, 4, 3}, {1, 3, 2, 4}, 
 	{1, 3, 4, 2}, {1, 4, 2, 3}, {1, 4, 3, 2}, 
 	{2, 1, 4, 3}, {2, 1, 3, 4}, {2, 3, 4, 1}, 
@@ -12,42 +14,108 @@ const int Maze::dir[24][4] = {
 };
 
 Maze::Maze(void)
-{
-
-}
-
-Maze::Maze(const Maze &copy)
-{
-
-}
+:_maze(), _width(0), _height(0), _gen_anime(false), _search_anime(false)
+{}
 
 Maze::~Maze(void)
-{
+{}
 
+// 미로 생성
+void	Maze::Maze_generate(int width, int height, bool anime)
+{
+	_width = width;
+	_height = height;
+	_gen_anime = anime;
+
+	srand(time(NULL));
+	//TODO: visit set
+	int start_i = rand() % width;
+	int start_j = rand() % height;
+	Maze_gen_DFS(start_i, start_j, anime);
+	//TODO: visit reset
+	//TODO: rerendering
 }
 
-Maze	&Maze::operator=(const Maze &copy)
+void	Maze::Maze_gen_DFS(int i, int j, bool anime)
 {
+	_maze[i][j].visit = 1;
+	/*
+	if (anime)
+	{
+		rerendering//
+	}
+	*/
 
-	return (*this);
+	int dir = rand() % NUM_DIR;
+	for (int idx = 0; idx < 4; idx++)
+	{
+		switch (_dir[dir][idx])
+		{
+		case W:
+			if (j == 0 || _maze[i][j - 1].visit)
+				break;
+			_maze[i][j].w = 1;
+			_maze[i][j - 1].s = 1;
+			Maze_gen_DFS(i, j - 1, anime);
+			break;
+		case S:
+			if (j == _height - 1 || _maze[i][j + 1].visit)
+				break;
+			_maze[i][j].s = 1;
+			_maze[i][j + 1].w = 1;
+			Maze_gen_DFS(i, j + 1, anime);
+			break;
+		case A:
+			if (i == 0 || _maze[i - 1][j].visit)
+				break;
+			_maze[i][j].a = 1;
+			_maze[i - 1][j].d = 1;
+			Maze_gen_DFS(i - 1, j, anime);
+			break;
+		case D:
+			if (i == _width - 1 || _maze[i + 1][j].visit)
+				break;
+			_maze[i][j].d = 1;
+			_maze[i + 1][j].a = 1;
+			Maze_gen_DFS(i + 1, j, anime);
+			break;
+		}
+	}
 }
 
-void	Maze::Maze_generate(int width, int height)
+// 미로 탐색
+void	Maze::Maze_search(int algo, int s_i, int s_j, int d_i, int d_j)
 {
-
+	switch (algo)
+	{
+	case DFS:
+		Maze_dfs_search(false, s_i, s_j, d_i, d_j);
+		break;
+	case DFS_ANIME:
+		Maze_dfs_search(true, s_i, s_j, d_i, d_j);
+		break;
+	case BFS:
+		Maze_bfs_search(false, s_i, s_j, d_i, d_j);
+		break;
+	case BFS_ANIME:
+		Maze_bfs_search(true, s_i, s_j, d_i, d_j);
+		break;
+	default:
+		break;
+	}
 }
 
-void	Maze::Maze_bfs_search(int x, int y)
+void	Maze::Maze_bfs_search(bool anime, int s_i, int s_j, int d_i, int d_j)
+{
+	// 보류
+}
+
+void	Maze::Maze_dfs_search(bool anime, int s_i, int s_j, int d_i, int d_j)
 {
 	
 }
 
-void	Maze::Maze_gen_DFS()
-{
-
-}
-
-void	Maze::Maze_search_DFS()
+void	Maze::Maze_search_DFS(int i, int j, bool anime)
 {
 	
 }
--- Maze.hpp
+++ Maze.hpp
@@ -3,29 +3,59 @@
 #ifndef MAZE_HPP
 # define MAZE_HPP
 
-#include "Space.hpp"
+# define NUM_DIR 24
+
+struct Space
+{
+	unsigned char w : 1;
+	unsigned char s : 1;
+	unsigned char a : 1;
+	unsigned char d : 1;
+	unsigned char visit : 1;
+	unsigned char search_visit : 1;
+};
+
+enum e_dir
+{
+	W = 1,
+	S = 2,
+	A = 3,
+	D = 4
+};
+
+enum e_algo
+{
+	DFS,
+	DFS_ANIME,
+	BFS,
+	BFS_ANIME
+};
 
 class Maze
 {
 public:
 	Maze(void);
-	Maze(const Maze &copy);
 	~Maze(void);
 
-	Maze	&operator=(const Maze &copy);
-	void	Maze_generate(int width, int height);
-	void	Maze_bfs_search(int x, int y);
-	void	Maze_dfs_search(int x, int y);
+	void	Maze_generate(int width, int height, bool anime);
+	void	Maze_search(int algo, int s_i, int s_j, int d_i, int d_j);
 private:
-	void	Maze_gen_DFS();
-	void	Maze_search_DFS();
+	void	Maze_bfs_search(bool anime, int s_i, int s_j, int d_i, int d_j);
+	void	Maze_dfs_search(bool anime, int s_i, int s_j, int d_i, int d_j);
+	void	Maze_gen_DFS(int i, int j, bool anime);
+	void	Maze_search_DFS(int i, int j, bool anime);
 
-	Space _maze[30][30];
-	bool _search_visit[30][30];
-	int _width;
-	int _height;
+	Space	_maze[30][30];
+	int		_width;
+	int		_height;
+	bool	_gen_anime;
+	bool	_search_anime;
 
-	static const int dir[24][4];
+	static const int _dir[24][4];
+
+	Maze	&operator=(const Maze &copy);
+	Maze(const Maze &copy);
+	// friend mfc
 };
 
 #endif
